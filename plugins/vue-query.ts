55616f83bdfb7562d6289b3aef0a633d05d51b35
ymc9 <104139426+ymc9@users.noreply.github.com>
2023-10-18 10:10:33 -0400

    update

--- plugins/vue-query.ts
+++ plugins/vue-query.ts
@@ -1,11 +1,6 @@
-import {
-    type VueQueryPluginOptions,
-    VueQueryPlugin,
-    QueryClient,
-} from '@tanstack/vue-query';
+import { VueQueryPlugin, QueryClient } from '@tanstack/vue-query';
 
 export default defineNuxtPlugin((nuxt) => {
     const queryClient = new QueryClient();
-    const options: VueQueryPluginOptions = { queryClient };
-    nuxt.vueApp.use(VueQueryPlugin, options);
+    nuxt.vueApp.use(VueQueryPlugin, { queryClient });
 });
