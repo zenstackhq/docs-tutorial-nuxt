e5aed0c30610bbf129a682b2ebfb937639380e34
ymc9 <104139426+ymc9@users.noreply.github.com>
2024-04-11 16:15:32 +0800

    update to latest zenstack

--- lib/hooks/__model_meta.ts
+++ lib/hooks/__model_meta.ts
@@ -1,97 +1,95 @@
 /* eslint-disable */
 const metadata = {
-    fields: {
+    models: {
         user: {
-            id: {
-                name: 'id',
-                type: 'String',
-                isId: true,
-                attributes: [{ name: '@default', args: [] }],
-            },
-            email: {
-                name: 'email',
-                type: 'String',
-            },
-            name: {
-                name: 'name',
-                type: 'String',
-            },
-            password: {
-                name: 'password',
-                type: 'String',
-            },
-            posts: {
-                name: 'posts',
-                type: 'Post',
-                isDataModel: true,
-                isArray: true,
-                backLink: 'author',
-            },
-        },
+            name: 'User', fields: {
+                id: {
+                    name: "id",
+                    type: "String",
+                    isId: true,
+                    attributes: [{ "name": "@default", "args": [] }],
+                }, email: {
+                    name: "email",
+                    type: "String",
+                }, name: {
+                    name: "name",
+                    type: "String",
+                }, password: {
+                    name: "password",
+                    type: "String",
+                }, posts: {
+                    name: "posts",
+                    type: "Post",
+                    isDataModel: true,
+                    isArray: true,
+                    backLink: 'author',
+                },
+            }
+            , uniqueConstraints: {
+                id: {
+                    name: "id",
+                    fields: ["id"]
+                }, email: {
+                    name: "email",
+                    fields: ["email"]
+                },
+            }
+            ,
+        }
+        ,
         post: {
-            id: {
-                name: 'id',
-                type: 'String',
-                isId: true,
-                attributes: [{ name: '@default', args: [] }],
-            },
-            createdAt: {
-                name: 'createdAt',
-                type: 'DateTime',
-                attributes: [{ name: '@default', args: [] }],
-            },
-            updatedAt: {
-                name: 'updatedAt',
-                type: 'DateTime',
-                attributes: [{ name: '@updatedAt', args: [] }],
-            },
-            title: {
-                name: 'title',
-                type: 'String',
-            },
-            content: {
-                name: 'content',
-                type: 'String',
-            },
-            published: {
-                name: 'published',
-                type: 'Boolean',
-                attributes: [{ name: '@default', args: [{ value: false }] }],
-            },
-            author: {
-                name: 'author',
-                type: 'User',
-                isDataModel: true,
-                backLink: 'posts',
-                isRelationOwner: true,
-                foreignKeyMapping: { id: 'authorId' },
-            },
-            authorId: {
-                name: 'authorId',
-                type: 'String',
-                isForeignKey: true,
-            },
-        },
-    },
-    uniqueConstraints: {
-        user: {
-            id: {
-                name: 'id',
-                fields: ['id'],
-            },
-            email: {
-                name: 'email',
-                fields: ['email'],
-            },
-        },
-        post: {
-            id: {
-                name: 'id',
-                fields: ['id'],
-            },
-        },
-    },
-    deleteCascade: {},
-    authModel: 'User',
+            name: 'Post', fields: {
+                id: {
+                    name: "id",
+                    type: "String",
+                    isId: true,
+                    attributes: [{ "name": "@default", "args": [] }],
+                }, createdAt: {
+                    name: "createdAt",
+                    type: "DateTime",
+                    attributes: [{ "name": "@default", "args": [] }],
+                }, updatedAt: {
+                    name: "updatedAt",
+                    type: "DateTime",
+                    attributes: [{ "name": "@updatedAt", "args": [] }],
+                }, title: {
+                    name: "title",
+                    type: "String",
+                }, content: {
+                    name: "content",
+                    type: "String",
+                }, published: {
+                    name: "published",
+                    type: "Boolean",
+                    attributes: [{ "name": "@default", "args": [{ "value": false }] }],
+                }, author: {
+                    name: "author",
+                    type: "User",
+                    isDataModel: true,
+                    backLink: 'posts',
+                    isRelationOwner: true,
+                    foreignKeyMapping: { "id": "authorId" },
+                }, authorId: {
+                    name: "authorId",
+                    type: "String",
+                    isForeignKey: true,
+                    relationField: 'author',
+                },
+            }
+            , uniqueConstraints: {
+                id: {
+                    name: "id",
+                    fields: ["id"]
+                },
+            }
+            ,
+        }
+        ,
+    }
+    ,
+    deleteCascade: {
+    }
+    ,
+    authModel: 'User'
 };
 export default metadata;
--- lib/hooks/index.ts
+++ lib/hooks/index.ts
@@ -1,4 +1,4 @@
 export * from './user';
 export * from './post';
-export { getQueryKey } from '@zenstackhq/tanstack-query/runtime';
-export { VueQueryContextKey, provideHooksContext } from '@zenstackhq/tanstack-query/runtime/vue';
+export { getQueryKey } from '@zenstackhq/tanstack-query/runtime-v5';
+export { VueQueryContextKey, provideHooksContext } from '@zenstackhq/tanstack-query/runtime-v5/vue';
